85ad4668dc7b4372175148d4c0fcc95eb3cdb1bf
Ariel Isaacs <ariel.isaacs@gmail.com>
2015-02-19 10:16:54 -0800

    add devise to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 
   validates :name, presence: true
   validates :phone_number, presence: true, uniqueness: true, format: { with: /[0-9]/, message: "only allows numbers"}
