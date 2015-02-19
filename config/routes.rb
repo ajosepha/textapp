19149818ab3a86af8aa978b05b8f6cc1a9bfb528
Ariel Isaacs <ariel.isaacs@gmail.com>
2015-02-19 11:59:10 -0800

    add sign out

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  devise_for :users
+
+
+  devise_for :users, :controllers => { registrations: 'registrations' }
+
 
   post 'twilio/voice' => 'twilio#voice'
 
