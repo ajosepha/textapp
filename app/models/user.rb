efb96b09df9c0f7f3fa38f0f5bcf9869c018c7c1
Ariel Isaacs <ariel.isaacs@gmail.com>
2015-02-23 11:55:20 -0800

    add messages table

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ActiveRecord::Base
 
   validates :phone_number, presence: true, uniqueness: true, format: { with: /[0-9]/, message: "only allows numbers"}
 
+  has_many :messages, dependent: :destroy
+
   def format_phone_number
     # @formatted_phone_number = number_to_phone(phone_number, country_code: 1)
   end
